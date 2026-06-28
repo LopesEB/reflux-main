152ac27da3576b4d34efcb744b9eb37394c8bbc5
Arthur Lopes dos Santos <backuparthur6@gmail.com>
2026-06-28 16:15:39 -0300

    Add files via upload

--- src/providers/redecanais/services/api.service.ts
+++ src/providers/redecanais/services/api.service.ts
@@ -1,12 +1,20 @@
 import { PROVIDER_URL } from '@/providers/redecanais/constants/url';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import * as https from 'https';
 
 @Injectable()
 export class RedeCanaisApiService {
   public readonly http = axios.create({
     baseURL: PROVIDER_URL,
-    headers: { referer: PROVIDER_URL },
+    headers: {
+      referer: PROVIDER_URL,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+      ciphers: 'ALL',
+    }),
   });
 
   public constructor() {}
